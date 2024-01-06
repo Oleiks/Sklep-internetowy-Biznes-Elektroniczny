ed755d0f79e645e9ac0d5eecd23c109a9dcc45a7
Aleksander Górski <89322216+Oleiks@users.noreply.github.com>
2024-01-06 18:25:52 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 MYSQL_HOST="db"
-MYSQL_PORT="3306"
 MYSQL_USER="root"
 MYSQL_PASSWORD="student"
 DATABASE_NAME="BE_188612"
-SQL_FILE="./sqldump.sql"
-
-if mysql -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "use $DATABASE_NAME"; then
-    mysql -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "DROP DATABASE $DATABASE_NAME";
+SQL_FILE="/docker-entrypoint-initdb.d/sqldump.sql"
+sleep 5
+if mysql -h "$MYSQL_HOST" -P 3306 -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "use $DATABASE_NAME"; then
+    mysql -h "$MYSQL_HOST" -P 3306 -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "DROP DATABASE $DATABASE_NAME";
 fi
-
-mysql -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "CREATE DATABASE $DATABASE_NAME";
-
-mysql -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$DATABASE_NAME" < "$SQL_FILE";
+sleep 5
+mysql -h "$MYSQL_HOST" -P 3306 -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "CREATE DATABASE $DATABASE_NAME";
+sleep 5
+mysql -h "$MYSQL_HOST" -P 3306 -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$DATABASE_NAME" < "$SQL_FILE";
+exec apache2-foreground
